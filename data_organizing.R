0618d307336040a9f14915b19f110f9a47627bbb
leosbarbosas <leosbarbosas@gmail.com>
2023-02-26 02:14:13 -0300

    add ibama data

--- data_organizing.R
+++ data_organizing.R
@@ -1,19 +1,11 @@
 library(geobr)
 library(sf)
-library(datazoom.amazonia)
-
-data <- load_ppm(dataset = "ppm_aquaculture",
-                 raw_data = FALSE,
-                 geo_level = "country",
-                 time_period = 2015:2018)
-deter_amz <- load_deter(dataset = 'deter_amz',
-                        raw_data = FALSE)
-
-
-
 library(ggplot2)
+library(dplyr)
 library(datazoom.amazonia)
+library(gt)
 
+# load, organize and save livestock data
 data <- load_ppm(dataset = "ppm_cow_farming",
                  raw_data = TRUE,
                  geo_level = "state",
@@ -26,9 +18,34 @@ data <- dplyr::filter(data,unidade_da_federacao %in% c('Pará', 'Mato Grosso','A
 
 write.csv(data,'dados/dados_pecuaria.csv')
 
-ggplot(data = data, aes(x = ano, y = valor, group = unidade_da_federacao, colour = unidade_da_federacao)) +
-  geom_line()
+
+# join livestock data with spatial data and save the result
+state <- read_state(code_state = 'all', year=2010, simplified=T) |>
+  dplyr::filter(abbrev_state %in% c('PA','MT','AM','RO','RR','AC','TO','MA','AP'))
+
+colnames(state)[3] <- 'unidade_da_federacao'
+
+data2015 <- filter(data,ano == 2015)
+
+state2015 <- left_join(state,data2015,by="unidade_da_federacao")
+
+dev.new()
+png(file="img/livestock_amz.png")
+plot(state2015['valor'],axes=T,key.pos = 1)
+dev.off()
 
 
-data2 <- load_ibama(dataset = "collected_fines", raw_data = FALSE,
-                   states = "BA", language = "pt")
+# get fines data
+fines <- load_ibama(dataset = "collected_fines", raw_data = FALSE,
+                   states = 'all', language = "pt") |> filter(uf %in% c('PA','MT','AM','RO','RR','AC','TO','MA','AP'))
+
+fines$count <- 1
+
+fines$valor_pago <- as.numeric(gsub(",", ".", gsub("\\.", "", fines$valor_pago)))
+
+fines_sum <- fines |> select(c(uf, valor_pago,count)) |> group_by(uf) |> summarise(across(c(valor_pago,count),(sum)))
+
+colnames(fines_sum) <- c('Estado','Valor total de multas pagas (1991-2023)','Total de multas aplicadas (1991-2023)')
+
+write.csv(fines_sum,'dados/multas.csv')
+
